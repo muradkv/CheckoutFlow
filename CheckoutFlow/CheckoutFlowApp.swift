6c2dee00d1e12cc0b548d81b1e6783e9287042c4
murad <muradkv@gmail.com>
2026-06-18 18:54:16 +0300

    Initial commit

---
+++ CheckoutFlow/CheckoutFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckoutFlowApp.swift
+//  CheckoutFlow
+//
+//  Created by murad on 18.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CheckoutFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckoutFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CheckoutFlow
+//
+//  Created by murad on 18.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
